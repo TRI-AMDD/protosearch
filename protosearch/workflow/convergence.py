bc5f92ad0c252609ad2da5a476eca2a571674bfd
Kirsten Winther <winther@stanford.edu>
2019-03-30 16:37:57 -0700

    initial convergence check module

--- protosearch/workflow/convergence.py
+++ protosearch/workflow/convergence.py
@@ -1,10 +1,91 @@
-from .workflow import Workflow
+import pylab as p
+import numpy as np
+from protosearch.utils.standards import Standards
 
+from .workflow import Workflow, PrototypeSQL
 
 class Convergence(Workflow):
     """TO DO: Automated convergence check """
 
-    def __init__(self,
-                 check_parameters=['encut', 'kspacing']):
+    def __init__(self):
 
         super().__init__()
+        self._connect()
+
+        self.fixed_convergence_params = ['encut', 'kspacing', 'ismear']
+
+        self.fix_params = {}
+        for p in self.fixed_convergence_params:
+            self.fix_params.update({p: Standards.calc_parameters[p]})
+        # index for highest converged point in numerically sorted array
+        self.convergence_index = {'encut': -1, 'kspacing': 0, 'ismear': 0}
+
+    def get_parametrized_energies(self, check_parameters=['encut', 'kspacing']):
+
+        total_energies = {}
+        for param in check_parameters:
+            param_st = Standards.calc_parameters[param]
+            prototypes = []
+            for d in self.ase_db.select('{}!={}'.format(param, param_st)):
+                if not d.p_name + '|' +  d.formula  in prototypes:
+                    prototypes.append(d.p_name + '|' + d.formula)
+            
+            total_energies[param] = {}
+            for proto in prototypes:
+                total_energies[param][proto] = {}
+                name, formula = proto.split('|')
+                fix_params = self.fix_params.copy()
+                del fix_params[param]
+                for d in self.ase_db.select(p_name=name,
+                                            relaxed=1,
+                                            formula=formula,
+                                            **fix_params):
+
+                    value = d.get(param, None)        
+                    total_energies[param][proto][value] = d.energy
+
+        return total_energies
+
+    def plot_convergence(self, check_parameters=['encut', 'kspacing']):
+
+        total_energies = self.get_parametrized_energies()
+
+        for param, proto_dict in total_energies.items():
+            p.figure()
+            p.title(param)
+            for proto, points in proto_dict.items():
+                x = np.array(list(points.keys()))
+                y = np.array(list(points.values()))
+
+                if len(x) <= 1:
+                    continue
+                
+                index = np.argsort(x)
+                x = x[index]
+                y = y[index]
+
+                y -= y[self.convergence_index[param]]
+                p.plot(x, np.zeros_like(x), 'r--')
+                
+                p.plot(x, y, '-*', label=proto)
+                p.ylim([-0.05, 0.05])
+                p.legend()
+        p.show()
+        
+        
+    def submit_convergence_check(self,
+                                 prototype,
+                                 check_parameters=['encut',
+                                                   'kspacing',
+                                                   'sigma']):    
+
+        parameter_mapping = {'encut': [300, 400, 500, 525, 550,
+                                       600, 700, 800],
+                             'kspacing': [10, 15, 20, 25, 30, 40],
+                             'sigma': [0.05, 0.1, 0.15, 0.2]}
+
+
+        for param in check_parameters:
+            for value in parameter_mapping[param]:
+                self.submit(prototype, calc_parameters={param: value})
+        
