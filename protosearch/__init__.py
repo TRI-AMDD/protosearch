6f76397794829137a2645f63702f9c09cf9af1ea
Kirsten Winther <winther@stanford.edu>
2019-04-23 10:20:48 -0700

    initial master function

--- protosearch/__init__.py
+++ protosearch/__init__.py
@@ -1,5 +1,19 @@
 import sys
 import os
 
+from protosearch.build_bulk.oqmd_interface import OqmdInterface
+
 __author__ = "TRI Materials"
 __version__ = '0.0.1'
+
+
+def start_learning(formula, max_atoms=None, source='oqmd'):
+
+    if source == 'oqmd':
+        StructureMaster = OqmdInterface
+        atoms_list = StructureMaster.get_structures(
+            formula, source='icsd', max_atoms=None)
+
+    else:
+        # Enumerate from prototypes
+        atoms_list = []
