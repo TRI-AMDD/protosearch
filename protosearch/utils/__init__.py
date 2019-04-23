466aaca8c8764d26d07a4e87a53176fdaf6f41bc
Kirsten Winther <winther@stanford.edu>
2019-04-23 10:39:57 -0700

    protosearch version in basepath

--- protosearch/utils/__init__.py
+++ protosearch/utils/__init__.py
@@ -2,6 +2,9 @@ __maintainer__ = "Kirsten Winther"
 
 import os
 
+from protosearch import __version__ as version
+
+
 def get_basepath(calculator='vasp',
                  tri_path=None,
                  username=None,
@@ -9,8 +12,14 @@ def get_basepath(calculator='vasp',
 
     TRI_PATH = tri_path or os.environ['TRI_PATH']
     username = username or os.environ['TRI_USERNAME']
-    basepath = TRI_PATH + '/model/{}/1/u/{}'\
-        .format(calculator, username)
+
+    # Protosearch version identification
+    protosearch_id = 'protosearch' + version.replace('.', '')
+
+    basepath = TRI_PATH + '/model/{}/1/u/{}/{}'\
+        .format(calculator, username, protosearch_id)
+    if not os.path.isdir(basepath):
+        os.mkdir(basepath)
     if ext:
         basepath += '/{}'.format(ext)
         if not os.path.isdir(basepath):
