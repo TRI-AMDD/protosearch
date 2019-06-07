e7ed8ad4b6b24c2afbb29fa85be475daa4a51a9c
Raul Flores <raulf2012@gmail.com>
2019-06-07 16:17:02 -0700

    Fix job status check where `errored` was being counted as `running`

--- protosearch/workflow/workflow.py
+++ protosearch/workflow/workflow.py
@@ -15,7 +15,7 @@ from .prototype_db import PrototypeSQL
 
 class Workflow(PrototypeSQL):
     """Submits calculations with TriSubmit, and tracks the calculations
-    in an ASE db. 
+    in an ASE db.
     """
 
     def __init__(self,
@@ -118,7 +118,7 @@ class Workflow(PrototypeSQL):
         Parameters:
         ----------
         map_species: dict
-          map A0, A1, etc to species. F.ex: {'A0': 'Fe', 'A1': 'O'} 
+          map A0, A1, etc to species. F.ex: {'A0': 'Fe', 'A1': 'O'}
         selection: dict
           ase db selection. F.ex: {'spacegroup': 166, natom=3}
         """
@@ -198,7 +198,7 @@ class Workflow(PrototypeSQL):
                 completed_ids += [calcid]
             elif status == 'errored':
                 failed_ids += [calcid]
-            elif status == 'errored':
+            elif status == 'running':
                 running_ids += [calcid]
 
         print('Status for calculations:')
