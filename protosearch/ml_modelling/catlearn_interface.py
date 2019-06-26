ee424a4fc0467f965ed7442c79b6bacc8939fdb8
Kirsten Winther <winther@stanford.edu>
2019-06-26 10:34:18 -0700

    fingerprinting fix

--- protosearch/ml_modelling/catlearn_interface.py
+++ protosearch/ml_modelling/catlearn_interface.py
@@ -1,27 +1,19 @@
 from catlearn.fingerprint.voro import VoronoiFingerprintGenerator
 from catlearn.preprocess.clean_data import clean_infinite, clean_variance
 
-#class CatlearnInterface:
-
-
-#    def __init__(self):
-#        pass
-
 
 def get_voro_fingerprint(atoms_list):
 
-    voro = VoronoiFingerprintGenerator(atoms_list)    
+    voro = VoronoiFingerprintGenerator(atoms_list)
     data_frame = voro.generate()
-    matrix = data_frame.as_matrix()
+    matrix = data_frame.values
 
     finite_numeric_data = clean_infinite(matrix)
+    #data = clean_variance(finite_numeric_data['train'])
 
-    data = clean_variance(finite_numeric_data['train'])
-    
-    return data
+    return finite_numeric_data['train']
 
 
-    
 def train(self):
     # Regression model
     pass
