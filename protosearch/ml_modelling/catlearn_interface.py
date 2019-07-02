5ca1ecd67ddf398f2d160199ba4c9b20e6efbbda
Kirsten Winther <winther@stanford.edu>
2019-07-02 10:55:47 -0700

    simple Gaussian Process regression with CatLearn

--- protosearch/ml_modelling/catlearn_interface.py
+++ protosearch/ml_modelling/catlearn_interface.py
@@ -1,5 +1,6 @@
 from catlearn.fingerprint.voro import VoronoiFingerprintGenerator
 from catlearn.preprocess.clean_data import clean_infinite, clean_variance
+from catlearn.regression.gaussian_process import GaussianProcess
 
 
 def get_voro_fingerprint(atoms_list):
@@ -14,6 +15,12 @@ def get_voro_fingerprint(atoms_list):
     return finite_numeric_data['train']
 
 
-def train(self):
-    # Regression model
-    pass
+def predict(train_features, train_target, test_features):
+    kernel = [{'type': 'gaussian', 'width': 1., 'scaling': 1., 'dimension': 'single'}]
+
+    GP = GaussianProcess(train_fp=train_features, train_target=train_target, kernel_list=kernel,
+                         regularization=1e-2, optimize_hyperparameters=True, scale_data=True)
+
+    pred = GP.predict(test_fp=test_features, uncertainty=True)
+
+    return pred
