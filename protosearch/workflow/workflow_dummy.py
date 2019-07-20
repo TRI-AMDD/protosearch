5837e936937fdcdb8a2e734a1c2a6b48b94c616c
Raul Flores <raulf2012@gmail.com>
2019-07-20 11:49:24 -0700

    Updates to DummyWorkflow class

--- protosearch/workflow/workflow_dummy.py
+++ protosearch/workflow/workflow_dummy.py
@@ -1,16 +1,9 @@
-import os
 import json
-import subprocess
 import time
-import ase
-from ase.io import read
-# import bulk_enumerator as be
 
 from protosearch.utils import get_basepath
-# from protosearch.build_bulk.build_bulk import BuildBulk
 from protosearch.build_bulk.classification import get_classification
-from protosearch.utils.standards import VaspStandards
-# from protosearch.calculate.submit import TriSubmit
+# from protosearch.utils.standards import VaspStandards
 from .prototype_db import PrototypeSQL
 
 
@@ -23,7 +16,7 @@ class DummyWorkflow(PrototypeSQL):
                  calculator='vasp',
                  db_filename=None,
                  basepath_ext=None,
-                 job_complete_time=0.8,
+                 job_complete_time=0.6,
                  ):
         """
         Args:
@@ -45,22 +38,10 @@ class DummyWorkflow(PrototypeSQL):
         #__|
 
 
-
-    def submit_enumerated(self, map_species, selection={}):
-        """
-        Submit a collection of enumeated prototypes.
-        Parameters:
-        ----------
-        map_species: dict
-          map A0, A1, etc to species. F.ex: {'A0': 'Fe', 'A1': 'O'}
-        selection: dict
-          ase db selection. F.ex: {'spacegroup': 166, natom=3}
-        """
-
-        prototypes = self.select(**selection)
-
-        for prototype in prototypes:
-            self.submit(prototype)
+    def recollect(self):
+        print("Not actually calling trisync here")
+        # subprocess.call('trisync', cwd=self.basepath)
+        # self.collected = True
 
     def submit_id_batch(self, calc_ids, ncpus=1, calc_parameters=None):
         """Submit a batch of calculations. Takes a list of atoms
@@ -74,7 +55,7 @@ class DummyWorkflow(PrototypeSQL):
         Submit an atomic structure by id
         """
         row = self.ase_db.get(id=int(calc_id))
-        atoms = row.toatoms()
+        # atoms = row.toatoms()
 
         formula = row.formula
         p_name = row.p_name
@@ -101,28 +82,14 @@ class DummyWorkflow(PrototypeSQL):
 
         self.ase_db.update(int(calc_id), **key_value_pairs)
 
-    def write_submission(self, key_value_pairs):
-        """Track submitted job in database"""
-        con = self.connection or self._connect()
-        self._initialize(con)
-
-        atoms = read(key_value_pairs['path'] + '/initial.POSCAR')
-
-        key_value_pairs.update({'relaxed': 0,
-                                'completed': 0,
-                                'submitted': 1})
-
-        self.ase_db.write(atoms, key_value_pairs)
-
     def check_submissions(self):
         """Check for completed jobs"""
-        # if not self.collected:
-        #     self._collect()
 
         con = self.connection or self._connect()
         self._initialize(con)
 
-        # Only keeps track of jobs in current iteration, resets after each batch
+        # Only keeps track of jobs in current iteration
+        # resets after each batch
         status_count = {'completed': 0,
                         'running': 0,
                         'errored': 0}
@@ -131,9 +98,6 @@ class DummyWorkflow(PrototypeSQL):
         failed_ids = []
         running_ids = []
         for d in self.ase_db.select(submitted=1, completed=0):
-            # path = d.path + '/simulation'
-            # calcid0 = d.id
-
             status, calcid = self.check_job_status(d)
 
             status_count[status] += 1
@@ -151,21 +115,12 @@ class DummyWorkflow(PrototypeSQL):
 
     def check_job_status(self, d):
         status = 'running'
-
         atoms = d.toatoms()
         calcid = d.id
 
-        # print("DISJFIDJ")
-        # print(d)
-        # print("DISJFIDJ")
-
         time_submit = d.submit_time
         time_i = time.time()
-
         time_elapsed = time_i - time_submit
-
-        # print("time_elapsed: ")
-        # print(time_elapsed)
 
         if time_elapsed > self.job_complete_time:
             # Job completed
@@ -174,13 +129,12 @@ class DummyWorkflow(PrototypeSQL):
 
             import copy
             from protosearch.utils.dummy_calc import DummyCalc
-
             atoms = copy.deepcopy(atoms)
             calc = DummyCalc(energy_zero=42.0)
             atoms.set_calculator(calc)
-            # calc.get_potential_energy()
 
             calcid = self.save_completed_calculation(atoms, calcid)
+
 
         else:
             # Job not completed
@@ -189,44 +143,11 @@ class DummyWorkflow(PrototypeSQL):
 
         return(status, calcid)
 
-        #| - out of sight
-        # for root, dirs, files in os.walk(path):
-        #     if 'monitor.sh' in files and not 'finalized' in files:
-        #         status = 'running'
-        #         break
-        #     elif 'monitor.sh' in files and 'completed' in files:
-        #         # Calculation completed - now save everything
-        #         try:  # Sometimes outcar is corrupted
-        #             atoms = ase.io.read(root + '/OUTCAR')
-        #             status = 'completed'
-        #             calcid = self.save_completed_calculation(atoms,
-        #                                                      path,
-        #                                                      root,
-        #                                                      calcid)
-        #         except:
-        #             print("Couldn't read OUTCAR")
-        #             status = 'errored'
-        #             self.save_failed_calculation(root, calcid)
-        #         break
-        #     elif 'monitor.sh' in files and 'err' in files:
-        #         status = 'errored'
-        #         self.save_failed_calculation(root, calcid)
-        #         break
-        #
-        # return status, calcid
-        #__|
-
-    def save_completed_calculation(self,
-        atoms,
-        # path,
-        # runpath,
-        calcid,
-        ):
+    def save_completed_calculation(self, atoms, calcid):
 
         self.ase_db.update(id=calcid,
                            completed=1)
 
-        # param_dict = params2dict(runpath + '/param')
         prototype, cell_parameters = get_classification(atoms)
 
         key_value_pairs = {'relaxed': 1,
@@ -240,13 +161,8 @@ class DummyWorkflow(PrototypeSQL):
 
         key_value_pairs.update(prototype)
         key_value_pairs.update(cell_parameters)
-        # key_value_pairs.update(param_dict)
 
         key_value_pairs = clean_key_value_pairs(key_value_pairs)
-
-        # atoms = set_calculator_info(atoms, param_dict)
-
-        # atoms.energy = 42  # TEMP
 
         newcalcid = self.ase_db.write(atoms, key_value_pairs)
         self.ase_db.update(id=calcid,
@@ -254,69 +170,9 @@ class DummyWorkflow(PrototypeSQL):
                            completed=1)
 
         return newcalcid
-
-    def save_failed_calculation(self, runpath, calcid):
-
-        key_value_pairs = {'completed': -1,
-                           'runpath': runpath}
-        param_dict = params2dict(runpath + '/param')
-        atoms = ase.io.read(runpath + '/initial.POSCAR')
-        prototype, cell_parameters = get_classification(atoms)
-
-        key_value_pairs.update(prototype)
-        key_value_pairs.update(cell_parameters)
-        key_value_pairs.update(param_dict)
-
-        key_value_pairs = clean_key_value_pairs(
-            key_value_pairs)
-
-        with open(runpath + '/err', 'r') as errorf:
-            message = errorf.read().replace("'", '')
-
-            self.ase_db.update(id=calcid,
-                               **key_value_pairs,
-                               data={'error': message})
-
-    def rerun_failed_calculations(self):
-        # self._collect()
-        con = self.connection or self._connect()
-        self._initialize(con)
-
-        for d in self.ase_db.select(completed=-1):
-            resubmit, handle = vasp_errors(error)
-            if not resubmit:
-                print('Job errored: {}'.format(handle))
-                self.ase_db.update(id=d.id, completed=-1)
-                continue
-            if handle == 'ncpus':
-                ncpus = 8
-            else:
-                ncpus = 1
-
-            p_name = d.p_name
-            path = d.path + '/simulation'
-
-            calc_parameters = {}
-            for param in VaspStandards.sorted_calc_parameters:
-                if d.get(param, None):
-                    calc_parameters.update({param: d.get(param)})
-
-            self.submit({'spacegroup': d.spacegroup,
-                         'wyckoffs': json.loads(d.wyckoffs),
-                         'species': json.loads(d.species)},
-                        ncpus=ncpus,
-                        calc_parameters=calc_parameters)
-
-            # Treat as completed, now it's resubmitted
-            self.ase_db.update(id=d.id, completed=1)
-
-    def get_completed_batch(self):
-        ids = self.check_submissions()
-
     #__|
 
 
-#| - Methods
 def clean_key_value_pairs(key_value_pairs):
     for key, value in key_value_pairs.items():
         if isinstance(value, list):
@@ -327,31 +183,3 @@ def clean_key_value_pairs(key_value_pairs):
         except:
             pass
     return key_value_pairs
-
-
-def set_calculator_info(atoms, parameters):
-
-    atoms.calc.name = 'vasp.5.4.4.18Apr17-6-g9f103f2a35'
-    atoms.calc.parameters = parameters
-
-    return atoms
-
-
-def vasp_errors(error):
-
-    if 'lattice_constant = float(f.readline().split()[0])' in error:
-        return False, 'Vasp failed'
-    elif 'Vasp exited' in error:
-        return True, 'ncpus'
-
-
-def params2dict(paramfile):
-    param_dict = {}
-    with open(paramfile, 'r') as f:
-        param = f.read().lstrip('/').rstrip('\n')
-        param_values = param.split('/')
-        for i, param_key in enumerate(VaspStandards.sorted_calc_parameters):
-            param_dict[param_key] = param_values[i]
-
-    return param_dict
-#__|
