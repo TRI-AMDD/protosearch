a0d56bba3a0ad715aa063b60fa5f8781cf96e951
Kirsten Winther <winther@stanford.edu>
2019-07-22 16:21:19 -0700

    normalize data, anisotropic scaling and better guess for width parameter

--- protosearch/ml_modelling/catlearn_interface.py
+++ protosearch/ml_modelling/catlearn_interface.py
@@ -1,5 +1,6 @@
 from catlearn.fingerprint.voro import VoronoiFingerprintGenerator
 from catlearn.preprocess.clean_data import clean_infinite, clean_variance
+from catlearn.preprocess.scaling import normalize
 from catlearn.regression.gaussian_process import GaussianProcess
 
 
@@ -10,16 +11,22 @@ def get_voro_fingerprint(atoms_list):
     matrix = data_frame.values
 
     finite_numeric_data = clean_infinite(matrix)
-    #data = clean_variance(finite_numeric_data['train'])
 
     return finite_numeric_data['train']
 
 
 def predict(train_features, train_target, test_features):
-    kernel = [{'type': 'gaussian', 'width': 1., 'scaling': 1., 'dimension': 'single'}]
 
-    GP = GaussianProcess(train_fp=train_features, train_target=train_target, kernel_list=kernel,
-                         regularization=1e-2, optimize_hyperparameters=True, scale_data=True)
+    data = clean_variance(train_features, test_features)
+    data = normalize(data['train'], data['test'])
+    train_features = data['train']
+    test_features = data['test']
+
+    kernel = [{'type': 'gaussian', 'width': 3, 'scaling': 1.}]
+
+    GP = GaussianProcess(train_fp=train_features, train_target=train_target,
+                         kernel_list=kernel, regularization=5e-2,
+                         optimize_hyperparameters=True, scale_data=True)
 
     pred = GP.predict(test_fp=test_features, uncertainty=True)
 
