a9ec07818a5d4ae90ecfb059d2e51258fff73467
Kirsten Winther <winther@stanford.edu>
2019-08-08 10:25:16 -0700

    clean feature will removes structure with broken fingerprint

--- protosearch/ml_modelling/fingerprint.py
+++ protosearch/ml_modelling/fingerprint.py
@@ -1,10 +1,9 @@
 import sys
 import copy
-
+import numpy as np
 import pandas as pd
 
 from catlearn.fingerprint.voro import VoronoiFingerprintGenerator
-
 from catlearn.preprocess.clean_data import (
     clean_infinite,
     clean_variance,
@@ -211,23 +210,36 @@ class VoronoiFingerprint:
         self.features = self.Voro_inst.generate()
 
 
-def clean_features(train_features, test_features, scale=False):
-    # get finite features
-    output = clean_infinite(train_features,
-                            test=test_features)
-    # Clean variance
-    output = clean_variance(output['train'],
-                            output['test'])
-    # Clean infinite
-    output = clean_infinite(output['train'],
-                            output['test'])
+def clean_features(features, scale=False):
+    remove_indices = {'train': np.array([], dtype=int),
+                      'test': np.array([], dtype=int)}
+    for key, feature_set in features.items():
+        if feature_set is None:
+            continue
+        bad_structure_indices = \
+            np.where(np.isfinite(feature_set).all(axis=1)==False)
+        for b in bad_structure_indices:
+            if len(np.where(np.isfinite(feature_set[b])==False)) > 1:
+                remove_indices[key] = np.append(remove_indices[key], b)
 
-    # Clean skewness
-    output = clean_skewness(output['train'],
-                            output['test'])
+        features[key] = np.delete(feature_set, remove_indices[key], axis=0)
+    if not 'test' in features:
+        features['test'] = None
+
+    # Finite features
+    features = clean_infinite(features['train'],
+                            features['test'])
+    # Clean variance
+    features = clean_variance(features['train'],
+                              features['test'])
+
+    #Clean skewness
+    features = clean_skewness(features['train'],
+                              features.get('test', None),
+                              skewness=3)
 
     if scale:
-        output = standardize(output['train'],
-                             output['test'])
+        features = standardize(features['train'],
+                            features.get('test', None))
 
-    return output['train'], output['test']
+    return features, remove_indices
