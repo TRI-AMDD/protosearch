489c4667a6443c825bb71937758ca4c99f21e3fc
Kirsten Winther <winther@stanford.edu>
2019-12-11 14:00:17 -0800

    add voronoi functionallity here for now since changes not in CatKit yet

--- protosearch/build_bulk/fitness_function.py
+++ protosearch/build_bulk/fitness_function.py
@@ -1,12 +1,15 @@
 import numpy as np
+import scipy
+from shapely.geometry import Polygon
 from ase.data import covalent_radii as cradii
-from catkit.gen.utils.connectivity.coordinates import expand_cell
+from catkit.gen.utils.coordinates import expand_cell
 
 from protosearch.utils.data import metal_numbers, prefered_O_state,\
     favored_O_connections, electronegs
 
 
-def get_area_neighbors(atoms, cell_cutoff=15, cutoff=None):
+def get_area_neighbors(atoms, cell_cutoff=15, cutoff=None,
+                       return_std_con=False):
     # Builds on top of CatKit connectivity matrix
     """Return the connectivity matrix from the Voronoi
     method weighted by the area of the Voronoi vertices.
@@ -38,13 +41,16 @@ def get_area_neighbors(atoms, cell_cutoff=15, cutoff=None):
     areas = []
     area_indices = []
     connectivity = np.zeros((len(atoms), len(atoms)))
-    connectivity_int = np.zeros((len(atoms), len(atoms)), dtype=int)
+    connectivity_int = np.zeros((len(atoms), len(atoms)))
     try:
         voronoi = scipy.spatial.Voronoi(coords, qhull_options='QbB Qc Qs')
         points = voronoi.ridge_points
     except:
         print('Voronoi failed')
-        return connectivity
+        if not return_std_con:
+            return connectivity
+        else:
+            return connectivity, connectivity_int
 
     Voro_cell = voronoi.points.ptp(axis=0)
     for i, n in enumerate(origional_indices):
@@ -78,7 +84,7 @@ def get_area_neighbors(atoms, cell_cutoff=15, cutoff=None):
 
                 vertices *= Voro_cell
 
-                area = get_weighted_area(vertices)
+                area = get_weighted_area(vertices, d[ii])
                 if area is None:
                     continue
 
@@ -89,9 +95,123 @@ def get_area_neighbors(atoms, cell_cutoff=15, cutoff=None):
             connectivity_int[v][u] += count
 
     connectivity /= 2
-    connectivity_int /= 2
 
-    return connectivity, connectivity_int
+    if not return_std_con:
+        return connectivity
+    else:
+        connectivity_int /= 2
+        return connectivity, connectivity_int.astype(int)
+
+
+def get_voronoi_neighbors(atoms, cutoff=5.0, return_distances=False):
+    """Return the connectivity matrix from the Voronoi
+    method. Multi-bonding occurs through periodic boundary conditions.
+
+    Parameters
+    ----------
+    atoms : atoms object
+        Atoms object with the periodic boundary conditions and
+        unit cell information to use.
+    cutoff : float
+        Radius of maximum atomic bond distance to consider.
+
+    Returns
+    -------
+    connectivity : ndarray (n, n)
+        Number of edges formed between atoms in a system.
+    """
+    index, coords, offsets = expand_cell(atoms, cutoff=cutoff)
+
+    xm, ym, zm = np.max(coords, axis=0) - np.min(coords, axis=0)
+
+    L = int(len(offsets) / 2)
+    origional_indices = np.arange(L * len(atoms), (L + 1) * len(atoms))
+
+    voronoi = scipy.spatial.Voronoi(coords, qhull_options='QbB Qc Qs')
+    points = voronoi.ridge_points
+
+    connectivity = np.zeros((len(atoms), len(atoms)))
+    distances = []
+    distance_indices = []
+    for i, n in enumerate(origional_indices):
+        ridge_indices = np.where(points == n)[0]
+        p = points[ridge_indices]
+        dist = np.linalg.norm(np.diff(coords[p], axis=1), axis=-1)[:, 0]
+        edges = np.sort(index[p])
+
+        if not edges.size:
+            warnings.warn(
+                ("scipy.spatial.Voronoi returned an atom which has "
+                 "no neighbors. This may result in incorrect connectivity."))
+            continue
+
+        unique_edge = np.unique(edges, axis=0)
+
+        for j, edge in enumerate(unique_edge):
+            indices = np.where(np.all(edge == edges, axis=1))[0]
+            d = dist[indices][np.where(dist[indices] < cutoff)[0]]
+            count = len(d)
+            if count == 0:
+                continue
+
+            u, v = edge
+
+            distance_indices += [sorted([u, v])]
+            distances += [sorted(d)]
+
+            connectivity[u][v] += count
+            connectivity[v][u] += count
+
+    connectivity /= 2
+    if not return_distances:
+        return connectivity.astype(int)
+
+    if len(distances) > 0:
+        distance_indices, unique_idx_idx = \
+            np.unique(distance_indices, axis=0, return_index=True)
+        distance_indices = distance_indices.tolist()
+
+        distances = [distances[i] for i in unique_idx_idx]
+
+    pair_distances = {'indices': distance_indices,
+                      'distances': distances}
+
+    return connectivity.astype(int), pair_distances
+
+
+def get_weighted_area(vertices, d):
+    center = vertices[0, :]
+    vertices -= center
+
+    # Plane of polygon in 3d defined by unit vectors ux and uy
+    ux = vertices[1, :].copy()
+    ux /= np.linalg.norm(ux)
+    vy = vertices[2, :].copy()
+
+    # ensure uy is orthogonal to ux
+    vyproj = np.dot(vy, ux) * ux
+    uy = vy - vyproj
+    uy /= np.linalg.norm(uy)
+
+    # Project all vectors onto plane to get 2D coordinates
+    Xs = np.dot(vertices, ux)
+    Ys = np.dot(vertices, uy)
+
+    poly_coords = [[Xs[i], Ys[i]] for i in range(len(Xs))]
+    polygon = Polygon(poly_coords)
+
+    try:
+        area = polygon.area
+    except:
+        return None
+
+    perimeter = polygon.length
+
+    # Scale area with bond distance and perimeter
+    weight = d * perimeter / 4
+    area /= weight
+
+    return area
 
 
 def get_covalent_density(atoms):
@@ -154,7 +274,9 @@ def get_fitness(atoms, use_density=True):
         #    return np.max([fitness, -20])
 
         # get connectivity matrix
-        con_matrix, con_int = get_area_neighbors(atoms)
+        con_matrix, con_int = get_area_neighbors(atoms,
+                                                 return_std_con=True)
+
         con_matrix_norm = con_matrix / con_int
         #print(np.round(con_matrix, 2))
         metal_idx = [i for i, a in enumerate(
