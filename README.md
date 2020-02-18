bfb2aad75e1ce79417f1ea854fc2e94659fb2526
Joseph Montoya <joseph.montoya@tri.global>
2020-02-18 11:00:39 -0800

    clean up setup and add install to readme

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Software for enumerating crystal structure prototypes to by used for active learning exploration with DFT and machine learning.
 
 ## Installation
-Note that the BulkEnumeration is currently available by request only and is required to enumerate prototypes from scratch. Experimental prototypes are parsed from the db file build_bulk/oqmd_icsd.db
+Using the setup script:
+
+```sh
+$ python setup.py install
+```
--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 from setuptools import setup, find_packages
-import warnings
 
 
-# TODO: resolve dependency on BulkEnumeration
 setup(name="protosearch",
       packages=find_packages(),
       install_requires=["ase>=3.17",
-                        "numpy>=1.14",
-                        # "BulkEnumeration>=0.2"
-                        ],
-      # dependency_links=["https://gitlab.com/ankitjainmeiitk/Enumerator.git"]
+                        "numpy>=1.14"],
       )
-
-if __name__ == '__main__':
-    try:
-        import bulk_enumerator as be
-    except ImportError:
-        warnings.warn("BulkEnumeration is required for proper functioning"
-                      "of protosearch, please request the code.")
