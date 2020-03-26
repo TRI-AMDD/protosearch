9262a5d88ac1d28edf2138d9106f64b208402231
Joseph Montoya <joseph.montoya@tri.global>
2020-03-25 23:51:22 -0700

    add catlearn

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(name="protosearch",
       packages=find_namespace_packages(),
       install_requires=["ase>=3.19",
                         "numpy>=1.14",
+                        "catlearn",
                         "shapely"],
       )
