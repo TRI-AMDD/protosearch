b838e614d250e4fd01d97fe79bec38d39af6a50f
Joseph Montoya <joseph.montoya@tri.global>
2020-05-09 11:58:21 -0700

    update protosearch reqs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_namespace_packages
 setup(name="protosearch",
       packages=find_namespace_packages(),
       install_requires=["ase>=3.19",
-                        "numpy>=1.14",
-                        "catlearn",
+                        "numpy>=1.18",
+                        "CatLearn>=0.6.2",
                         "shapely"],
       )
